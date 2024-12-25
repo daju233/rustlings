a7cabf6c912aeaf03fc6c4a2a80a2d0c6d67434c
daju233 <2212509690@qq.com>
2024-12-26 01:44:49 +0800

    update

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -24,6 +24,9 @@ fn calculate_price_of_apples(number: i32) -> i32 {
     }
 }
 
+fn main() {
+    // You can optionally experiment here.
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
